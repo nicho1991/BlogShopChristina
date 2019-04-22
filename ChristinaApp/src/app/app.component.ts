4ffb7c13015036b251aa3f546bb99909a9c44cc1
Nicholas <ladefoged1991@gmail.com>
2019-04-22 15:35:46 +0200

    First draft of quill implemented

--- ChristinaApp/src/app/app.component.ts
+++ ChristinaApp/src/app/app.component.ts
@@ -1,10 +1,93 @@
 import { Component } from '@angular/core';
 
+import * as QuillNamespace from 'quill';
+const Quill: any = QuillNamespace;
+import ImageResize from 'quill-image-resize-module';
+Quill.register('modules/imageResize', ImageResize);
+
+import { DomSanitizer } from '@angular/platform-browser';
+
+var BaseImageFormat = Quill.import('formats/image');
+const ImageFormatAttributesList = [
+    'alt',
+    'height',
+    'width',
+    'style'
+];
+
+
+class ImageFormat extends BaseImageFormat {
+  static formats(domNode) {
+    return ImageFormatAttributesList.reduce(function(formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+  format(name, value) {
+    if (ImageFormatAttributesList.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+
+Quill.register(ImageFormat, true);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChristinaApp';
+  quillHTML: any;
+  public editorModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ header: 1 }, { header: 2 }],               // custom button values
+      [{ list: 'ordered'}, { list: 'bullet' }],
+      [{ script: 'sub'}, { script: 'super' }],      // superscript/subscript
+      [{ indent: '-1'}, { indent: '+1' }],          // outdent/indent
+      [{ direction: 'rtl' }],                         // text direction
+
+      [{ size: ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ color: [] }, { background: [] }],          // dropdown with defaults from theme
+      [{ font: [] }],
+      [{ align: [] }],
+
+      ['clean'],                                         // remove formatting button
+
+      ['link', 'image', 'video']                         // link and image, video
+    ],
+      imageResize: true
+    };
+
+    public showerModules = {
+      toolbar: false,
+    };
+    constructor(private sanitizer: DomSanitizer) {
+
+    }
+    public output(event) {
+      this.quillHTML = this.sanitizer.bypassSecurityTrustHtml(event.html);
+      /* 
+        editor: editorInstance, // Quill
+        html: html, // html string
+        text: text, // plain text string
+        content: content, // Content - operatins representation
+        delta: delta, // Delta
+        oldDelta: oldDelta, // Delta
+        source: source // ('user', 'api', 'silent' , undefined)
+      */
+    }
 }
--- ChristinaApp/src/app/app.module.ts
+++ ChristinaApp/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+
+import { QuillModule } from 'ngx-quill';
+
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +23,9 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase, 'my-app-name'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    QuillModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
